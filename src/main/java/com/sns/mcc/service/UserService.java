9181b5dd37c18357157a45a760052b5adc18d885
guluming <guluming@naver.com>
2022-08-22 22:30:41 +0900

    Feat: "추가 로그인 API 구현"
    - 기존에 회원가입시 받던 nickname을 첫로그인시 입력하도록 변경
    - nickname의 경우 회원정보 수정 API에서 입력
    - 추가된 concern테이블의 존재여부로 첫로그인인지 아닌지를 판단하여 boolen값으로 응답
    - 토큰의 재발급시 accesstoken이 유효하지만 기간만 지난 토큰이고, refreshtoken이 서버에 저장되어있는 토큰과 같을 경우 재발급
    - refreshtoken이 서버에 없으면 비로그인으로 간주
    - refreshtoken도 유효날짜가 지났을경우 재로그인 요청 응답

--- src/main/java/com/sns/mcc/service/UserService.java
+++ src/main/java/com/sns/mcc/service/UserService.java
@@ -1,14 +1,25 @@
 package com.sns.mcc.service;
 
 import com.sns.mcc.dto.request.*;
+import com.sns.mcc.dto.response.JwtResponse;
 import com.sns.mcc.dto.response.UserDuplicateResponse;
+import com.sns.mcc.dto.response.UserLoginResponse;
+import com.sns.mcc.entity.Auth;
+import com.sns.mcc.entity.Concern;
 import com.sns.mcc.entity.User;
+import com.sns.mcc.enums.ErrorCode;
 import com.sns.mcc.enums.UserRole;
+import com.sns.mcc.repository.AuthRepository;
+import com.sns.mcc.repository.ConcernRepository;
 import com.sns.mcc.repository.UserRepository;
+import com.sns.mcc.security.UserDetailsImpl;
+import com.sns.mcc.security.jwt.JwtTokenProvider;
 import com.sns.mcc.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,17 +31,18 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+    private final ConcernRepository concernRepository;
+    private final AuthRepository authRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
     //회원가입
     @Transactional
     public ResponseEntity<Object> userSignup(UserSignUpRequest userSignUpRequest) {
         boolean usernameCheck = this.duplicateCheckUsername(userSignUpRequest.getUsername()).isExist();
-        boolean nicknameCheck = this.duplicateCheckNickname(userSignUpRequest.getNickname()).isExist();
         if (usernameCheck) {
             return new ResponseEntity<>("사용 불가능한 아이디 입니다.", HttpStatus.BAD_REQUEST);
-        } else if (nicknameCheck) {
-            return new ResponseEntity<>("사용 불가능한 닉네임 입니다.", HttpStatus.BAD_REQUEST);
         } else if (!userSignUpRequest.getPassword().equals(userSignUpRequest.getPasswordChk())) {
             return new ResponseEntity<>("비밀번호와 재확인 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
         } else {
@@ -76,16 +88,97 @@ public class UserService {
     }
 
     //로그인
-    public ResponseEntity<?> userLogin(UserLoginRequest userLoginRequest) {
+    public ResponseEntity<Object> userLogin(UserLoginRequest userLoginRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword()));
+        } catch (Exception e) {
+            return new ResponseEntity<>(ErrorCode.USERNAME_OR_PASSWORD_NOTFOUND.getMessage(), ErrorCode.USERNAME_OR_PASSWORD_NOTFOUND.getStatus());
+        }
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        Optional<User> user = userRepository.findByUsername(userLoginRequest.getUsername());
+        if (user.isEmpty()) {
+            return new ResponseEntity<>("등록되지 않은 회원입니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(jwtTokenCreate(user.get()), HttpStatus.CREATED);
     }
 
     //회원정보 수정
     @Transactional
-    public ResponseEntity<?> userEdit(UserEditRequest userEditRequest) {
+    public ResponseEntity<Object> userEdit(UserDetailsImpl userDetails, UserEditRequest userEditRequest) {
+        boolean nicknameCheck = this.duplicateCheckUsername(userEditRequest.getNickname()).isExist();
+        if (nicknameCheck) {
+            return new ResponseEntity<>("사용 불가능한 닉네임 입니다.", HttpStatus.BAD_REQUEST);
+        }
 
+        Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
+
+        if (user.isEmpty()) {
+            return new ResponseEntity<>("등록되지 않은 회원 입니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        if (user.get().getConcern() == null) {
+            final Concern emptyConcern = concernRepository.save(new Concern());
+            user.get().newUserUpdate(userEditRequest.getNickname(), emptyConcern);
+        } else {
+            user.get().userUpdate(userEditRequest.getNickname());
+        }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    //Access 토큰 재발급
+    public ResponseEntity<?> newAccessToken(NewTokenRequest newTokenRequest) {
+        JwtResponse jwtResponse = new JwtResponse();
+
+        Optional<Auth> refreshToken = authRepository.findByRefreshToken(newTokenRequest.getRefreshtoken());
+        if (refreshToken.isEmpty()) {
+            jwtResponse.setCode(ErrorCode.UNAUTHORIZED.getCode());
+            jwtResponse.setMessage(ErrorCode.UNAUTHORIZED.getMessage());
+            jwtResponse.setErrorStstus(ErrorCode.UNAUTHORIZED.getStatus());
+            return new ResponseEntity<>(jwtResponse, HttpStatus.UNAUTHORIZED);
+        }
+
+        if (jwtTokenProvider.validateToken(refreshToken.get().getRefreshToken())) {
+
+            Optional<User> user = userRepository.findByUsername(refreshToken.get().getUsername());
+            if (user.isEmpty()) {
+                return new ResponseEntity<>(" 등록되지 않은 회원 입니다.", HttpStatus.BAD_REQUEST);
+            }
+
+            String accessToken = jwtTokenProvider.createAccessToken(user.get());
+            jwtResponse.setAccessToken(accessToken);
+            jwtResponse.setRefreshToken(newTokenRequest.getRefreshtoken());
+            jwtResponse.setMessage("새로운 AccessToken이 발급 되었습니다.");
+        }
+
+        return new ResponseEntity<>(jwtResponse, HttpStatus.CREATED);
+    }
+
+    //JWT 토큰 생성기
+    public UserLoginResponse jwtTokenCreate(User user) {
+        UserLoginResponse userLoginResponse = new UserLoginResponse();
+
+        if (user.getConcern() == null) {
+            String accessToken = jwtTokenProvider.createAccessToken(user);
+            String refreshToken = jwtTokenProvider.createRefreshToken(user);
+
+            Auth auth = new Auth(user.getUsername(), refreshToken);
+            authRepository.save(auth);
+
+            userLoginResponse.setAccessToken(accessToken);
+            userLoginResponse.setRefreshToken(refreshToken);
+            userLoginResponse.setFirst(true);
+        } else {
+            String accessToken = jwtTokenProvider.createAccessToken(user);
+            String refreshToken = jwtTokenProvider.createRefreshToken(user);
+
+            Auth auth = new Auth(user.getUsername(), refreshToken);
+            authRepository.save(auth);
+
+            userLoginResponse.setAccessToken(accessToken);
+            userLoginResponse.setRefreshToken(refreshToken);
+        }
+        return userLoginResponse;
+    }
 }
