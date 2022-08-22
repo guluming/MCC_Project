e4aefe4345c39af47e02f5e780f3d7b5367da39f
guluming <guluming@naver.com>
2022-08-22 19:47:32 +0900

    Feat: "추가 전역 예외 처리 구현"
    - 회원가입시 형식 예외 처리를 작성하면서 이후에 생길 다른 예외 처리들(로그인 등) 공통으로 관리하기 위해
    - ErrorCode enum 클래스에 여러 에러 관련 속성 추가

--- src/main/java/com/sns/mcc/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sns/mcc/handler/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.sns.mcc.handler;
 
+import com.sns.mcc.dto.response.ErrorResponse;
+import com.sns.mcc.enums.ErrorCode;
+import com.sns.mcc.exception.CustomSignUpException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomSignUpException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorResponse> handleCustomSignUpException(CustomSignUpException ex){
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.SIGN_UP_DATA_VALID.getCode(), ErrorCode.SIGN_UP_DATA_VALID.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
