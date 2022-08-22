01105f464851a37ce6ce50a6e0d12683fa56221a
guluming <guluming@naver.com>
2022-08-22 22:18:59 +0900

    Feat: "추가 Spring Security를 이용한 인증,인가 구현"
    - Spring Security를 사용하여 인증,인가를 구현
    - 이제 필요한 UserDetails, UserDetailsService 상속받아 클래스 구현
    - 다만 추가적으로 jwt를 구현할 것이기 때문에 session사용해제

--- src/main/java/com/sns/mcc/security/UserDetailsImpl.java
+++ src/main/java/com/sns/mcc/security/UserDetailsImpl.java
@@ -1,4 +1,68 @@
 package com.sns.mcc.security;
 
-public class UserDetailsImpl {
+import com.sns.mcc.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final UserRole role;
+
+    public UserDetailsImpl(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRole role = getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();           //List인 이유 : 여러개의 권한을 가질 수 있다
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/sns/mcc/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sns/mcc/security/UserDetailsServiceImpl.java
@@ -1,4 +1,27 @@
 package com.sns.mcc.security;
 
-public class UserDetailsServiceImpl {
+import com.sns.mcc.entity.User;
+import com.sns.mcc.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("등록되지 않는 사용자 입니다.");
+        }
+        return new UserDetailsImpl(user.get().getUsername(), user.get().getPassword(), user.get().getRole());
+    }
 }
