2c9790e886f66ee4e68c1dd0006893f98f3f8bab
guluming <guluming@naver.com>
2022-08-24 23:36:13 +0900

    Feat: "추가 concern 클래스 작성"
    - 게임, 영화, 음악, 연애, 도서 5가지 종류
    - 랜덤채팅을 이용한 후에 해당 필드값을 변경해서
    - 이후 랭킹시스템이나 자신이 가장 많이 한 주제확인 등으로 이용

--- src/main/java/com/sns/mcc/entity/Concern.java
+++ src/main/java/com/sns/mcc/entity/Concern.java
@@ -4,16 +4,35 @@ import com.sns.mcc.utill.TimeStamped;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@NoArgsConstructor
 @Getter
 @Entity
 public class Concern extends TimeStamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private int game;
+
+    @Column
+    private int movie;
+
+    @Column
+    private int music;
+
+    @Column
+    private int love;
+
+    @Column
+    private int book;
+
+    public Concern() {
+        this.game = 0;
+        this.movie = 0;
+        this.music = 0;
+        this.love = 0;
+        this.book = 0;
+    }
 }
